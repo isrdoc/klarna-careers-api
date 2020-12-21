b446f6a49c66fe2b3cff9fb71dcb32b2d316c0d5
Igor Srdoc <igor@srdoc.si>
2020-12-21 23:19:04 +0100

    Add People service. Disable demo services

--- services/index.js
+++ services/index.js
@@ -1,18 +1,22 @@
 export default [
+  // {
+  //   name: 'accounts',
+  //   port: 4001,
+  // },
+  // {
+  //   name: 'inventory',
+  //   port: 4002,
+  // },
+  // {
+  //   name: 'products',
+  //   port: 4003,
+  // },
+  // {
+  //   name: 'reviews',
+  //   port: 4004,
+  // },
   {
-    name: 'accounts',
-    port: 4001,
-  },
-  {
-    name: 'inventory',
-    port: 4002,
-  },
-  {
-    name: 'products',
-    port: 4003,
-  },
-  {
-    name: 'reviews',
-    port: 4004,
+    name: 'people',
+    port: 4005,
   },
 ]
---
+++ services/people/schema.js
@@ -0,0 +1,54 @@
+import {
+  makeExecSchema,
+  makeStichingTypeDefs,
+  NotFoundError,
+} from '../../utils/index.js'
+
+const people = [
+  { id: '1', firstName: 'Igor', lastName: 'Srdoc' },
+  { id: '2', firstName: 'Alexandre', lastName: 'Borel' },
+]
+
+// export interface Person {
+//   id: string
+//   firstName: string
+//   lastName: string
+
+//   story?: Story
+//   title?: Title
+// }
+
+const schema = `#graphql
+  type Person @key(selectionSet: "{ id }") {
+    id: ID!
+    firstName: String!
+    lastName: String!
+  }
+
+  type Query {
+    person(id: ID!): Person @merge(keyField: "id")
+    people(ids: [ID!]!): [Person]! @merge(
+      keyField: "id"
+      keyArg: "ids"
+    )
+    _sdl: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    person: (_root, { id }) =>
+      people.find((person) => person.id === id) || new NotFoundError(),
+    people: (_root, { ids }) =>
+      ids.map(
+        (id) =>
+          people.find((person) => person.id === id) || new NotFoundError(),
+      ),
+    _sdl: () => typeDefs,
+  },
+}
+
+const typeDefs = makeStichingTypeDefs({ schema })
+const executableSchema = makeExecSchema({ schema, resolvers })
+
+export default executableSchema
